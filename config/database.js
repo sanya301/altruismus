7c3b37baa561232d52d54d72292662b0a7e6eb49
Sanya Nijhawan <sanya301@gmail.com>
2017-04-09 20:44:46 +0000

    restoring heroku

--- config/database.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-
-    'url' : 'mongodb://altruismus:altruismus123@ds155130.mlab.com:55130/altruismus' 
-
-};
--- server.js
+++ server.js
@@ -4,36 +4,6 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 8080;
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash = require('connect-flash');
-
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-
-var configDB = require('./config/database.js');
-
-// Configuration ===========================================================================================================
-mongoose.connect(configDB.url); // connects to the database
-
-// require('./config/passport')(passport); //pass passport for configuration
-
-// set up express
-app.use(logger('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
-
-// required for passport
-//app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
-//app.use(passport.initialize());
-//app.use(passport.session()); // persistent login sessions
-//app.use(flash()); // use connect-flash for flash messages stored in session
-
-// Routes =======================================================================================
-//require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configure
-
 
 // Launch =======================================================================================
 app.listen(port, function() {
@@ -46,8 +16,6 @@ app.use(express.static(__dirname + '/static'));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-
-
 
 // // set the home page route
 app.get('/', function(req, res) {
