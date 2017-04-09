8ab6bd0f94c207651a27c0db517fef67669bacfd
Sanya Nijhawan <sanya301@gmail.com>
2017-04-09 19:46:16 +0000

    restoring server

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var app = express();
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/static'));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/static'));
+
 
 // set the home page route
 app.get('/', function(req, res) {
