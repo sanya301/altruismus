84625e02a367c56237ecfd4ba7dfdeeef5d5437f
Sanya Nijhawan <sanya301@gmail.com>
2017-04-10 04:10:52 +0000

    omg I think this actually works

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    'url' : 'mongodb://a1:altruismus1@ds157380.mlab.com:57380/altruismus'
+
+};
--- server.js
+++ server.js
@@ -4,11 +4,69 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var MONGODB_URI = require('./config/database.js');
+
+var mongoOp     =   require("./model/mongo");
+
+var db = new mongoOp();
+var VUSERS_COLLECTION = "users";
+
+
+// Configuration ===========================================================================================================
+ mongoose.connect(MONGODB_URI.url, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  
+  // Save database object from the callback for reuse.
+  //db = database;
+  console.log("Database connection ready" + database);
+
+  // Initialize the app.
+    app.listen(port, function() {
+     	console.log('Our app is running on http://localhost:' + port);
+  });
+});
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+//mongoose.connect(MONGODB_URI.url); // connects to the database
+
+// require('./config/passport')(passport); //pass passport for configuration
+
+// set up express
+app.use(logger('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.urlencoded({extended: true})); // get information from html forms
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// Routes =======================================================================================
+//require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configure
+
 
 // Launch =======================================================================================
-app.listen(port, function() {
-	console.log('Our app is running on http://localhost:' + port);
-});
+//app.listen(port, function() {
+//	console.log('Our app is running on http://localhost:' + port);
+//});
 
 // OLD CODE BELOW  =======================================================================================
 // make express look in the public directory for assets (css/js/img)
@@ -16,6 +74,8 @@ app.use(express.static(__dirname + '/static'));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+
+
 
 // // set the home page route
 app.get('/', function(req, res) {
@@ -73,3 +133,74 @@ app.get('/userview.ejs', function(req, res) {
 	// ejs render automatically looks in the views folder
 	res.render('userview');
 });
+
+/*app.post ('/vsignup.ejs', function (req, res) {
+  
+//  console.log(req.body);
+  
+   db.collection('altruismus').save( req.body, function (err, result) {
+    if (err) return console.log(err);
+
+    console.log('saved to database');
+    res.redirect('/');
+   });
+}); */
+
+
+app.post("/vsignup.ejs", function(req, res) {
+/*  var newV = req.body;
+  newV.createDate = new Date();
+
+  if (!(req.body.firstname || req.body.lastname)) {
+    handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
+  }
+
+  db.collection(VUSERS_COLLECTION).insertOne(newV, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new user.");
+    } else {
+    //  res.status(201).json(doc.ops[0]);
+      console.log('saved to database');
+    }
+  }); */
+  
+    var response = {};
+    // fetch email and password from REST request.
+    // Add strict validation when you use this in Production.
+    db.email = req.body.email; 
+    db.firstname = req.body.firstname;
+    db.lastname = req.body.lastname;
+    db.dob = req.body.dob;
+    // Hash the password using SHA1 algorithm.
+    db.password =  require('crypto')
+                          .createHash('sha1')
+                          .update(req.body.password)
+                          .digest('base64');
+    db.confirmpassword =  require('crypto')
+                          .createHash('sha1')
+                          .update(req.body.confirmpassword)
+                          .digest('base64');
+    db.save(function(err){
+    // save() will run insert() command of MongoDB.
+    // it will add new data in collection.
+      if(err) {
+            response = {"error" : true,"message" : "Error adding data"};
+      } 
+      
+      else if (db.password != db.confirmpassword ) {
+            response = {"error" : true,"message" : "Passwords don't match"};
+      }
+      
+      
+      else {
+            response = {"error" : false,"message" : "Data added"};
+      }
+      res.json(response);
+      
+      
+      res.redirect('/');
+
+    });
+});
+  
+      
